edfbf6fd17365b7a1dcba507d2cadd08822764d2
Jacob Mitchinson <jmitchinson+github@gmail.com>
2014-12-09 16:34:07 +0000

    added methods & tests to boris_bikes

---
+++ spec/bike_spec.rb
@@ -0,0 +1,17 @@
+require 'bike'
+
+describe Bike do 
+
+	it 'should not be broken' do 
+		bike = Bike.new
+		expect(bike).not_to be_broken
+	end
+
+	it 'should be able to break' do 
+		bike = Bike.new
+		bike.break! 
+		expect(bike).to be_broken
+	end
+
+	
+end
